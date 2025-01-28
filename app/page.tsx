a4fb4e0728286db50584f412bf37413fa8fdfd9c
Dax Schoof <55069309+daxschoof@users.noreply.github.com>
2025-01-28 08:39:44 -0500

    BUGFIX - Fixing Image Size for Screen Size

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex-1 bg-gradient-to-br from-[#eadabe] to-[#1d314e] from-30% content-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 h-screen w-screen">
-      <div className="absolute top-4 right-4 rounded-3xl overflow-hidden">
+      <div className="absolute top-4 right-4 rounded-3xl overflow-hidden max-sm:w-[100] max-sm:h-[100]">
         <Image
           src="/Sparring_Sessions_Logo.jpg"
           alt="Sparring Sessions Logo"
@@ -13,36 +13,36 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col items-center justify-center gap-20">
-        <h1 className="sm:text-6xl md:text-5xl text-4xl sm:font-bold font-extrabold text-[#1d324f] min-h-14 w-screen text-center overflow-hidden">
+        <h1 className="max-sm:text-4xl text-6xl font-bold max-sm:font-extrabold text-[#1d324f] min-h-14 w-screen text-center overflow-hidden">
           Sparring Sessions Podcast
         </h1>
 
-        <div className="flex flex-row items-center justify-center gap-16">
+        <div className="flex flex-row items-center justify-center gap-32 max-sm:gap-8 max-md:gap-16">
           <a
             href="https://www.youtube.com/@SparringSessionsPod"
             target="_blank"
-            className="flex items-center gap-2 w-auto h-auto"
+            className="flex items-center gap-2 w-auto h-auto max-sm:max-h-[80] max-sm:max-w-[80] max-md:h-[120] max-md:w-[120]"
           >
-            <Image src="/Youtube.png" alt="Youtube" width={100} height={100} />
+            <Image src="/Youtube.png" alt="Youtube" width={140} height={140} />
           </a>
           <a
             href="https://www.instagram.com/sparring_sessions"
             target="_blank"
-            className="flex items-center gap-2"
+            className="flex items-center gap-2 max-sm:max-h-[60] max-sm:max-w-[60] max-md:h-[100] max-md:w-[100]"
           >
             <Image
               src="/instagram.png"
               alt="Instagram"
-              width={80}
-              height={80}
+              width={120}
+              height={120}
             />
           </a>
           <a
             href="https://open.spotify.com/show/4EJycwHyG2T2aH1K0YIatk"
             target="_blank"
-            className="flex items-center gap-2"
+            className="flex items-center gap-2 max-sm:max-h-[60] max-sm:max-w-[60] max-md:h-[100] max-md:w-[100]"
           >
-            <Image src="/spotify.png" alt="Spotify" width={80} height={80} />
+            <Image src="/spotify.png" alt="Spotify" width={120} height={120} />
           </a>
         </div>
       </div>
