ca519827d2c0dc1d2c208d895039506f4b73cba7
Dax Schoof <55069309+daxschoof@users.noreply.github.com>
2025-01-28 09:04:39 -0500

    FEATURE - Made Background + Image on Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Image from "next/image";
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,7 +29,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen h-screen overflow-clip`}
       >
-        {children}
+        <div className="flex-1 bg-gradient-to-br from-[#eadabe] to-[#1d314e] from-30% content-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 h-screen w-screen">
+          <div className="fixed top-4 right-4 rounded-3xl overflow-hidden max-sm:w-[100] max-sm:h-[100]">
+            <Image
+              src="/Sparring_Sessions_Logo.jpg"
+              alt="Sparring Sessions Logo"
+              width={150}
+              height={150}
+            />
+          </div>
+          {children}
+        </div>
       </body>
     </html>
   );
