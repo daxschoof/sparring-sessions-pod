4a33b7672ec1dea9e17ecc53f740cb241cc0401c
Dax Schoof <55069309+daxschoof@users.noreply.github.com>
2025-01-28 23:18:29 -0500

    FEATURE - Making Meter functional, reusable, and adding features

--- app/early-stoppage-meter/page.tsx
+++ app/early-stoppage-meter/page.tsx
@@ -1,35 +1,33 @@
 "use client";
 
 import { Meter } from "@/components/Meter";
-import { useState } from "react";
+import useSound from "use-sound";
 
 const EarlyStoppageMeter = () => {
-  const [meterValue, setMeterValue] = useState(0);
+  const [iAmNotImpSound] = useSound("/audio/I_Am_Not_Impressed.mp3");
+  const [andTwistItSound] = useSound("/audio/AND_TWIST_IT.mp3");
+  const [cantLetYouSound] = useSound("/audio/I_Can't_Let_You_Get_Close.mp3");
+  const [masvidalKOSound] = useSound("/audio/Masvidal_KO.mp3");
+  const [herbDeanFaultSound] = useSound("/audio/Herb_Dean_Fault.mp3");
 
   const earlyStoppageValues = [
-    { setValue: 6, display: 1 },
-    { setValue: 27, display: 2 },
-    { setValue: 50, display: 3 },
-    { setValue: 73, display: 4 },
-    { setValue: 94, display: 5 },
-    { setValue: -30, display: "Reset" },
+    { value: 10, display: 1, audioHook: iAmNotImpSound },
+    { value: 30, display: 2, audioHook: andTwistItSound },
+    { value: 50, display: 3, audioHook: cantLetYouSound },
+    { value: 70, display: 4, audioHook: masvidalKOSound },
+    { value: 90, display: 5, audioHook: herbDeanFaultSound },
+    { value: 0, display: "Reset" },
   ];
 
   return (
-    <div className="flex flex-col items-center justify-center w-full">
-      <Meter meterValue={meterValue} />
-
-      <div className="flex justify-center gap-4 mt-8">
-        {earlyStoppageValues.map((val) => (
-          <button
-            className="px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700"
-            onClick={() => setMeterValue(val.setValue)}
-            key={val.display}
-          >
-            {val.display}
-          </button>
-        ))}
-      </div>
+    <div className="flex flex-col items-center justify-center w-full mt-24">
+      <h1
+        className="text-6xl font-extrabold text-[#d22f36] capitalize"
+        style={{ fontFamily: "'Comic Sans', cursive" }}
+      >
+        YE' OLDE EARLY STOPPAGE METER
+      </h1>
+      <Meter values={earlyStoppageValues} />
     </div>
   );
 };
--- components/Meter.tsx
+++ components/Meter.tsx
@@ -1,77 +1,134 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 const GaugeComponent = dynamic(() => import("react-gauge-component"), {
   ssr: false,
 });
 
-interface IMeterProps {
-  meterValue: number;
+interface IValue {
+  value: number;
+  display: number | string;
+  audioHook?: any;
 }
 
-export const Meter = ({ meterValue }: IMeterProps) => {
+interface IMeterProps {
+  values: IValue[];
+}
+
+export const Meter = ({ values }: IMeterProps) => {
+  // TO-DO: ADD GIF OPTION FOR VALUES
+  // TO-DO: ADD MOBILE WARNING/STOPPER
+
+  const [buttonsShowing, setButtonsShowing] = useState(true);
+  const [meterValue, setMeterValue] = useState(0);
+
+  useEffect(() => {
+    document.addEventListener("keydown", onKeyDown, true);
+
+    return document.removeEventListener("keydown", onKeyDown);
+  });
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    const key = e.key;
+
+    if (key.toUpperCase() === "R") setMeterValue(0);
+    else if (key.toUpperCase() === "H") setButtonsShowing(!buttonsShowing);
+    else {
+      const displayKeyValue = values.filter(
+        (val) => val.display.toString() === key || val.value.toString() === key
+      );
+
+      if (displayKeyValue.length > 0) handleButtonClick(displayKeyValue[0]);
+    }
+  };
+
+  const handleButtonClick = (val: IValue) => {
+    setMeterValue(val.value);
+    if (val.audioHook) val.audioHook();
+  };
+
   return (
-    <GaugeComponent
-      type="semicircle"
-      labels={{
-        valueLabel: {
-          hide: true,
-        },
-        tickLabels: {
-          type: "inner",
-          ticks: [
-            { value: 10 },
-            { value: 30 },
-            { value: 50 },
-            { value: 70 },
-            { value: 90 },
-          ],
-          defaultTickValueConfig: {
-            formatTextValue: (value: number) => {
-              if (value === 0 || value === 100) return "";
-              return Math.round(value / 20).toString();
+    <>
+      <GaugeComponent
+        type="semicircle"
+        labels={{
+          valueLabel: {
+            hide: true,
+          },
+          tickLabels: {
+            type: "inner",
+            ticks: values.map((val) => {
+              return { value: val.value };
+            }),
+            defaultTickValueConfig: {
+              formatTextValue: (value: number) => {
+                if (value === 0 || value === 100) return "";
+                return Math.round(value / 20).toString();
+              },
+              style: { fontSize: "28", color: "black", fill: "black" },
             },
-            style: { fontSize: "28", color: "black", fill: "black" },
           },
-        },
-      }}
-      style={{
-        width: "1500px",
-        height: "-webkit-fill-available",
-        overflow: "visible",
-      }}
-      arc={{
-        width: 0.2,
-        subArcs: [
-          {
-            limit: 20,
-            color: "#EA4228",
-            showTick: false,
-          },
-          {
-            limit: 40,
-            color: "#F58B19",
-            showTick: false,
-          },
-          {
-            limit: 60,
-            color: "#F5CD19",
-            showTick: false,
-          },
-          {
-            limit: 80,
-            color: "#a5e12c",
-            showTick: false,
-          },
-          {
-            limit: 100,
-            color: "#5BE12C",
-            showTick: false,
-          },
-        ],
-      }}
-      pointer={{ width: 10, length: 0.55 }}
-      value={meterValue}
-    />
+        }}
+        style={{
+          width: "1500px",
+          height: "-webkit-fill-available",
+          overflow: "visible",
+          minHeight: "700px",
+        }}
+        arc={{
+          width: 0.2,
+          subArcs: [
+            {
+              limit: 20,
+              color: "#EA4228",
+              showTick: false,
+            },
+            {
+              limit: 40,
+              color: "#F58B19",
+              showTick: false,
+            },
+            {
+              limit: 60,
+              color: "#F5CD19",
+              showTick: false,
+            },
+            {
+              limit: 80,
+              color: "#a5e12c",
+              showTick: false,
+            },
+            {
+              limit: 100,
+              color: "#5BE12C",
+              showTick: false,
+            },
+          ],
+        }}
+        pointer={{ width: 10, length: 0.55 }}
+        value={meterValue}
+      />
+
+      {buttonsShowing && (
+        <div className="flex justify-center gap-4 mt-8">
+          {values.map((val) => (
+            <button
+              className="px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700"
+              onClick={() => handleButtonClick(val)}
+              key={val.display}
+            >
+              {val.display}
+            </button>
+          ))}
+          <button
+            className="px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700"
+            onClick={() => setButtonsShowing(false)}
+          >
+            Hide
+          </button>
+        </div>
+      )}
+    </>
   );
 };
